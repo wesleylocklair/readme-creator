f3457140af5c8a3ecdf2150b83de3ac6b373ffd0
Boss Eggs <wesley.j.locklair@gmail.com>
2024-07-07 15:31:18 -0400

    created the project!

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# readme-creator
+
+# README-Creator
+## Description
+Creating README's made simple. If youre someone like me and struggle to make readme's this is the project for you. I created this to make formatting and creating a readme file very simple. As long as you have node on your computer you are all set to go. In the future I would like to make it a little more user friendly with some of the options and selections. I am proud of how it turned out for now. Will most definetly be using this in the future for creating README's. 
+## Credits
+Website Name:Inquirer NPM-
+URL:https://www.npmjs.com/package/inquirer#prompt-types
+
+## License
+MIT License
+## Contact Me:
+My GitHub Account:
+https://github.com/wesleylocklair
+
+My Email Address:
+wesley.j.locklair@gmail.com
+
+## Video Demonstration
+
+[Video Demo](<../../../../Downloads/Untitled_ Jul 7, 2024 3_15 PM.webm>)
--- index.js
+++ index.js
@@ -3,10 +3,79 @@ const inquirer = require('inquirer')
 const colors = require('colors')
 
 inquirer
-    .prompt([
-        {
-        type:'input' ,
-        message: "Howre you?" ,
-        name: "answer"
-        }     
-    ])
+  .prompt([
+    {
+      type: 'input',
+      message: 'Whats the title of your README?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Whats your project about?',
+      name: 'description1',
+    },
+    {
+        type: 'input',
+        message: 'What inspired you to make this?',
+        name: 'description2',
+      },
+      {
+        type: 'input',
+        message: 'Any plans for future development?',
+        name: 'description3',
+      },
+    {
+      type: 'input',
+      message: 'Please list outside source names if you used any. Type N/A if none:',
+      name: 'credits1',
+    },
+    {
+        type: 'input',
+        message: 'Please copy/paste urls for credits here.(RC to paste):',
+        name: 'credits2',
+      },
+     {
+      type: 'list',
+      message: 'what license did you use?',
+      choices: ["MIT License",'Apache License 2.0','BSD 2-Clause "Simplified" License','The Unlicense',"Mozilla Public License 2.0", "None"], 
+      name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+      },
+      {
+        type: 'input',
+        message: 'What is your best contact email?',
+        name: 'email',
+      },
+  ])
+  .then((response) => {
+    const myreadme = `
+# ${response.title}
+## Description
+${response.description1}.${response.description2}.${response.description3}
+## Credits
+Website Name:${response.credits1}-
+
+URL:${response.credits2}
+
+## License
+${response.license}
+## Contact Me:
+
+My GitHub Account:
+https://github.com/${response.github}
+
+My Email Address:
+${response.email}
+    `
+        fs.writeFile('README.md', myreadme, (err) => { 
+            err ? console.error(err) : console.log('README.md Generated!')})
+       
+  });
+
+ 
+
+
